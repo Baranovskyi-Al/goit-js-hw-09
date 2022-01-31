56d50558c75db600226499cee4a382ce7ccd7076
Oleksandr Baranovskyi <89201309+Baranovskyi-Al@users.noreply.github.com>
2022-01-31 14:13:03 +0200

    add notiflix

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,2 +1,41 @@
 import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
+
+import Notiflix from 'notiflix';
+
+// Настраиваем выбор даты (для отрезания дат меньше текущей использовать можно "minDate: 'today'")
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    console.log(selectedDates[0]);
+  },
+};
+
+flatpickr('#datetime-picker', options);
+
+// Функция для подсчета значений
+
+// function convertMs(ms) {
+//   // Number of milliseconds per unit of time
+//   const second = 1000;
+//   const minute = second * 60;
+//   const hour = minute * 60;
+//   const day = hour * 24;
+
+//   // Remaining days
+//   const days = Math.floor(ms / day);
+//   // Remaining hours
+//   const hours = Math.floor((ms % day) / hour);
+//   // Remaining minutes
+//   const minutes = Math.floor(((ms % day) % hour) / minute);
+//   // Remaining seconds
+//   const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+//   return { days, hours, minutes, seconds };
+// }
+
+// Notiflix.Notify.failure('Please choose a date in the future');
