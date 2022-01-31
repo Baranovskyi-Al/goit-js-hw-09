a185f4b7591393c1a77eca830e8abcd7f2058a4d
Oleksandr Baranovskyi <89201309+Baranovskyi-Al@users.noreply.github.com>
2022-01-31 12:20:06 +0200

    complete task1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,39 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+// Добавляем кнопки
+
+const startButton = document.querySelector('[data-start]');
+const stopButton = document.querySelector('[data-stop]');
+
+// Сделаем кнопку СТОП неактивной по умолчанию для "чайников"
+
+startButton.disabled = false;
+stopButton.disabled = true;
+
+// Вешаем слушатели
+
+startButton.addEventListener('click', swinchingColor);
+stopButton.addEventListener('click', stopSwinchingColor);
+
+// Функция для кнопки старт
+
+function swinchingColor() {
+  const intervalDelay = 1000;
+
+  switchingInterval = setInterval(() => {
+    const randomColor = getRandomHexColor();
+    document.body.style.backgroundColor = randomColor;
+  }, intervalDelay);
+
+  startButton.disabled = true;
+  stopButton.disabled = false;
+}
+
+// Функция для кнопки стоп
+
+function stopSwinchingColor() {
+  clearInterval(switchingInterval);
+  startButton.disabled = false;
+}
